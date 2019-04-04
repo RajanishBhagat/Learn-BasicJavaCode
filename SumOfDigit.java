3acbcbc6dfdfc002d5c1abe4d501132e8ff57ace
Rajanish Bhagat <33940172+RajanishBhagat@users.noreply.github.com>
2019-04-04 17:52:17 +0530

    Commit done

--- SumOfDigit.java
+++ SumOfDigit.java
@@ -2,36 +2,31 @@
 
 import java.util.Scanner;
 
-class SumOfDigit 
-
+class SumOfDigit
 {
-
 public static void main(String args[])
-
 {
 
 int n, rem,  sum = 0;
 
-System.out.println("Enter the number to reverse ");
-
+System.out.println("Enter the  number ");
 Scanner sct = new Scanner(System.in);
-
 n = sct.nextInt(); //User Input
 
-while( n != 0 )// Reversing a Number Entered
-
+while( n != 0 )// check number should not equal to zero
 {
-
 rem = n%10;
-
 sum = sum + rem;
-
 n = n/10;
-
 }
 
-System.out.println("Reverse of entered number is "+sum);
-
+System.out.println("Sum of the digit is  "+sum);
+}
 }
 
-}
+/*
+rajanish@netstorm-TECRA-C50-C:~/Rajanish/InfoBase/java$ java SumOfDigit
+Enter the  number 
+123
+Sum of the digit is  6
+*/
