bdd5166176e3692863c1d3bb9735cab7482c99ba
Rajanish Bhagat <33940172+RajanishBhagat@users.noreply.github.com>
2020-06-17 19:12:48 +0530

    Update ArraysClass.java

--- ArraysClass.java
+++ ArraysClass.java
@@ -186,5 +186,29 @@ $java -Xmx128M -Xms16M LargestNumberSample
 sorted Array ::[10, 20, 25, 57, 63, 96]
 largest element is ::96
 
+// Find the Duplicate Element  in Array
+  public class DuplicateElement {
+   public static void main(String args[]){
+      int temp, size;
+      int array[] = {10, 20, 25, 63, 25, 96, 57};
+      size = array.length;
+      
+      for(int i = 0; i<size; i++ ){
+         for(int j = i+1; j<size; j++){
+             
+            if(array[i] == array[j]){
+              System.out.println("Duplicate element is = " + array[j]);
+            }
+              
+         }
 
+         
+      }
+
+   }
+}
+
+$javac DuplicateElement.java
+$java -Xmx128M -Xms16M DuplicateElement
+Duplicate element is = 25
 
